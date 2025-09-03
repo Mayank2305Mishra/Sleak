1c65550ef6f17dbb5a33ad2fcdd48135fe0920c7
Mayank Mishra <mayank2305mishra@gmail.com>
2025-09-03 19:36:57 +0000

    SETUP

--- sleak/app/page.tsx
+++ sleak/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="relative min-h-[100svh] flex items-center justify-center px-4 pb-[calc(env(safe-area-inset-bottom)+96px)] md:pb-[44vh]">
-      <h1 className="text-5xl parisienne-regular">Hello World</h1>
+      <h1 className="text-5xl parisienne-regular">Hello Mayank</h1>
     </div>
   );
 }
